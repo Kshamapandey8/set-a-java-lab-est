8c09cc3fb68caea25af0410cac6e3eb6b19d41e2
Kshamapandey8 <167602278+Kshamapandey8@users.noreply.github.com>
2025-11-13 12:18:47 +0530

    Update ShowEmployees.java

--- ShowEmployees.java
+++ ShowEmployees.java
@@ -1,1 +1,25 @@
+import java.sql.*;
 
+public class ShowEmployees {
+    public static void main(String[] args) {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con = DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:xe", "system", "password");
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM employee");
+
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("id"));
+                System.out.println("Name: " + rs.getString("name"));
+                System.out.println("Salary: " + rs.getDouble("salary"));
+                System.out.println("------------------------");
+            }
+
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
